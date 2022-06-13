b2d6ee596413d8d16e285267b74759e4196da540
Marcel Humes <mhumes@concentrichx.com>
2022-06-13 13:15:16 -0400

    intitial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const FILMS_QUERY = gql`
+  {
+    launchesPast(limit: 30) {
+      id
+      mission_name
+      launch_site {
+        site_name_long
+      }
+    }
+  }
+
+`;
+
+//const LAUNCHPADS_QUERY = gql`
+  //{
+    //launchpads(limit: 30) {
+    // location
+   // }
+ // }
+
+//`;
+
+export default function App() {
+  const { data, loading, error } = useQuery(FILMS_QUERY);
+
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>
+
+  return (
+    <div>
+      <h1>SpaceX Launches</h1>
+      <ul>
+        {data.launchesPast.map((launch) => (
+          <li key={launch.id}>{launch.mission_name} <br /> <div style={{display: "inline-block", paddingLeft:"0px"}}> {launch.launch_site.site_name_long}</div></li>
+        ))}  
+      </ul>
+    </div>
+  );
+}
